7f72c0aedeac435203377772fc949183613ece5b
uhey22e <uhey22e@users.noreply.github.com>
2021-05-02 12:11:33 +0900

    Fix storybooks

---
+++ stories/ExtraFoodInput.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { ExtraFoodInput } from "../src/components/ExtraFoodInput";
+
+export default {
+  title: "ExtraFoodInput",
+  component: ExtraFoodInput,
+};
+
+export const basicUsage = () => (
+  <ExtraFoodInput
+    ingredient={{
+      quantity: 10,
+      food: {
+        id: "oil",
+        shortName: "油",
+        detailName: "油",
+        unitGram: 1,
+        protein: 0,
+        fat: 1,
+        carbs: 0,
+        availableUnits: [],
+      },
+    }}
+    onChange={action("onChange")}
+  />
+);
