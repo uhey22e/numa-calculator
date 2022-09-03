79b0c03a50b406b809ac09070e82599c5863f5f1
uhey22e <uhey22e@users.noreply.github.com>
2022-09-03 18:35:42 +0900

    Upgrade gatsby, disable google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,15 +23,15 @@ module.exports = {
       },
     },
     // Google analytics
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     // The property ID; the tracking code won't be generated without it
+    //     trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "",
+    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //     head: true,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-google-adsense`,
       options: {
